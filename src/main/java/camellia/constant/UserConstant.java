0468050f3b5d195bacef93295a5426223f2b1fe9
Camellia.xiaohua <2421195723@qq.com>
2024-07-11 15:39:32 +0800

    常量类

---
+++ src/main/java/camellia/constant/UserConstant.java
@@ -0,0 +1,24 @@
+package camellia.constant;
+
+/**
+ * @Datetime: 2024/7/11下午3:34
+ * @author: Camellia.xioahua
+ */
+public interface UserConstant {
+    /**
+     * 用户登入态键
+     */
+
+    String USER_LOGIN_STATE = "userLoginState";
+    /**
+     * 普通用户权限
+     */
+
+    int DEFAULT_ROLE = 0;
+    /**
+     * 管理员用户权限
+     */
+
+    int ADMIN_ROLE = 1;
+
+}
