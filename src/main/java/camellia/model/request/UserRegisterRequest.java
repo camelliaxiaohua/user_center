9ea8bd47986f406721ce1164165718ae36b34bf5
Camellia.xiaohua <2421195723@qq.com>
2024-07-11 10:56:22 +0800

    用户注册请求体

---
+++ src/main/java/camellia/model/request/UserRegisterRequest.java
@@ -0,0 +1,23 @@
+package camellia.model.request;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 用户注册请求体
+ * @Datetime: 2024/7/11上午10:20
+ * @author: Camellia.xioahua
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -5631184043973031395L;
+
+    private String userAccount;
+
+    private String userPassword;
+
+    private String checkPassword;
+}
